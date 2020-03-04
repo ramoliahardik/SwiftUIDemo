d22debfbe09496338673380ed23d8553d520c1fe
ramoliahardik <49195916+ramoliahardik@users.noreply.github.com>
2020-03-04 06:35:38 +0530

    Initial Commit

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Hardik Ramolia on 04/03/20.
+//  Copyright © 2020 Hardik Ramolia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
